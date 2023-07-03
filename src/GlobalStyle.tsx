75d79e5dc86f575e3e01ba99b628c5344a619bd7
chiman <rlaclghks123@naver.com>
2023-07-03 10:46:06 +0900

    Chore: 초기설정

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    
+    body{
+        font-family:serif;
+    };
+
+    a{
+       text-decoration: none;
+    }
+    
+
+`;
+
+export default GlobalStyle;
