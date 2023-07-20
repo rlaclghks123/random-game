dbf1805d975f4cec2b36c6312bb7dd2ae6a7d732
chiman <rlaclghks123@naver.com>
2023-07-20 16:22:12 +0900

    Chore: 테스트를 위한 초기 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import type { InlineConfig } from 'vitest';
+import type { UserConfig } from 'vite';
 
-// https://vitejs.dev/config/
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
+
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
+    setupFiles: './setupTests.ts',
+  },
+} as VitestConfigExport);
