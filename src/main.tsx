927cf177c9b6bb1c463927ff5d9404b3cf5631f3
chiman <rlaclghks123@naver.com>
2023-07-23 21:40:27 +0900

    Refactor: MSW 실행부분 주석처리

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle.tsx';
 import Routers from './Routers.tsx';
-import initMocks from './mocks/index.ts';
+// import initMocks from './mocks/index.ts';
 
-if (process.env.NODE_ENV === 'development') {
-  await initMocks();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   await initMocks();
+// }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
