b3034882af811f52e7d6a21708a63e18ce8c7b76
chiman <rlaclghks123@naver.com>
2023-08-27 14:25:01 +0900

    Feat: Error Page 추가

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Game from './pages/Game';
+import ErrorPage from './pages/ErrorPage';
 
 const Routers = () => {
   return (
     <Routes>
       <Route path={'/'} element={<Home />}></Route>
       <Route path={'/game'} element={<Game />}></Route>
+      <Route path={'/*'} element={<ErrorPage />}></Route>
     </Routes>
   );
 };
--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,11 +1,31 @@
+import { styled } from 'styled-components';
 import Header from '../../components/Header';
 import MainPage from '../../layout/MainPage';
+
+const MainContainer = styled.section`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Span = styled.p`
+  margin: 20px 0px;
+  color: white;
+  font-size: 25px;
+`;
 
 const ErrorPage = () => {
   return (
     <MainPage>
       <Header />
-      <div>바이</div>
+      <MainContainer>
+        <Span>잘못된 페이지 요청입니다.</Span>
+        <Span>홈페이지로 이동해주세요</Span>
+      </MainContainer>
     </MainPage>
   );
 };
